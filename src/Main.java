316d41aca40dd74362aa678f42876b36beb41696
artur <mauboroda.arturtu@gmail.com>
2024-12-16 13:03:47 +0200

    perfect commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        /*System.out.print("Hello World");
         System.out.println(")");
-        System.out.println("Hello World ");
+        System.out.println("Hello World ");*/
+
 
 
     }
